b6bc9c7c18727b4c9a5ca095f8e975bd9075da2f
Thabet1295 <fsd5802@gmail.com>
2022-01-25 13:20:35 +0200

    footer component

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -5,6 +5,15 @@ const sass = gulpSass(require('sass'));
 
 // create task 
 
-gulp.task('sass',async function(){
-    gulp.src('src/components/**/*.scss').pipe(sass()).pipe(gulp.dest('src/css'))
-});
+// gulp.task('sass',async function(){
+//     gulp.src('src/components/**/*.scss').pipe(sass()).pipe(gulp.dest('src/css'))
+// });
+
+
+// watch scss chamging 
+
+gulp.task('watch',async function(){
+    gulp.watch('src/components/**/*.scss',async function(){
+        gulp.src('src/components/**/*.scss').pipe(sass()).pipe(gulp.dest('src/css'));
+    })
+})
---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "../../css/Footer/Footer.css";
+
+function Footer()
+{
+    return (
+       <footer>
+          &copy; Copyright Udemy Course;
+       </footer>
+    )
+}
+
+export default Footer;
